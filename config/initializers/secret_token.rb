7a89b8b96ba32e721f6106d778f48353c59d33d8
Paul Ingalls <paulingalls@hotmail.com>
2012-03-23 12:06:03 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FanzoSite::Application.config.secret_token = 'c4b5d1fcd757236ba97ddb9e2eecc68f05cf45d899a3fad264a3c970671492769d19d0f45c6e3be8b724e219b3382609c43650be681b2913b9205f1e8097fa1b'
