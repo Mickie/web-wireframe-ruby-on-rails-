ef423e56b60139f4b5961e9d2cf7a9a9368f8e88
Paul Ingalls <paulingalls@hotmail.com>
2012-03-23 17:35:26 -0700

    added devise and views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,23 @@ FanzoSite::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.raise_delivery_errors = false
+  
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  # change to false to prevent email from being sent during development
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"  
+  config.action_mailer.smtp_settings = {
+  address: "smtpout.secureserver.net",
+  port: 80,
+  domain: "fanzo.co",
+  authentication: "plain",
+  user_name: ENV["FANZO_USERNAME"],
+  password: ENV["FANZO_PASSWORD"]
+}
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
