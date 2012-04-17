c96c2133a21ba34afcad6a9c9681d536ce31b7c2
Paul Ingalls <paul@paulingalls.com>
2012-04-17 15:50:26 -0700

    make sure that a venue has a venue type

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -4,6 +4,7 @@ class Venue < ActiveRecord::Base
   belongs_to :venue_type
 
   validates :name, uniqueness:true, presence:true
+  validates :venue_type, presence:true
 
   accepts_nested_attributes_for :location
   accepts_nested_attributes_for :social_info
--- spec/models/venue_spec.rb
+++ spec/models/venue_spec.rb
@@ -13,4 +13,9 @@ describe Venue do
   it { should respond_to(:venue_type) }
   it { should respond_to(:location) }
   it { should respond_to(:social_info) }
+  
+  it "should require a venue type" do
+    @venue.venue_type_id = nil
+    @venue.should_not be_valid
+  end
 end
