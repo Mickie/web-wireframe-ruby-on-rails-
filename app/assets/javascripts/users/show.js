365de057801997a43f360fd71b075229c4766607
Paul Ingalls <paul@paulingalls.com>
2012-05-27 20:12:22 -0700

    use the new TeamHelper in the user show view

--- app/assets/javascripts/users/show.js
+++ app/assets/javascripts/users/show.js
@@ -1,21 +1,5 @@
-$(function(){
-  $("#league_picker").change(function(e) 
-  {
-    theLeagueId = e.target.value;
-    $.getJSON( "/teams.json?league_id=" + theLeagueId, onTeamsReady );
-  });
-  $("#event_id").change(function(e)
-  {
-    theGame = e.target.value;
-    document.location = "/events/" + theGame;
-  })
-})
+var myTeamHelper = new TeamHelper("#league_picker", "#user_team_team_id");
 
-function onTeamsReady(aResult)
-{
-  $("#user_team_team_id").empty();
-  for(var i=0,j=aResult.length; i<j; i++)
-  {
-    $("#user_team_team_id").append('<option value=' + aResult[i].id + '>' + aResult[i].name + '</option>');
-  };
-}
+$(function(){
+  myTeamHelper.connectToLeaguePicker();  
+})
