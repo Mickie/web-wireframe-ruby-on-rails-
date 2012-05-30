b6b4b4289853e70603aea851ece81d5373e9cf57
Paul Ingalls <paul@paulingalls.com>
2012-05-29 17:02:04 -0700

    speed up venues, playing with users show page.

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -4,7 +4,7 @@ class VenuesController < ApplicationController
   # GET /venues
   # GET /venues.json
   def index
-    @venues = Venue.all
+    @venues = Venue.includes(:location => :state).all
 
     respond_to do |format|
       format.html # index.html.erb
