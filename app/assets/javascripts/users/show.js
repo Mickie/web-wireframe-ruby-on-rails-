57a73ce34f8a4beba4965c4196d0f1da8d00a564
Paul Ingalls <paul@paulingalls.com>
2012-06-21 15:51:37 -0700

    removed duplication

--- app/assets/javascripts/users/show.js
+++ app/assets/javascripts/users/show.js
@@ -1,32 +1,8 @@
 var myTeamHelper = new TeamHelper("#league_picker", "#user_team_team_id");
 
-function onLoadDataComplete(aResult)
-{
-  $("#frameContent").html(aResult);
-}
-
-function onLoadError(anError)
-{
-  console.log(anError);  
-}
-
-function loadData(aPath)
-{
-  $.ajax({
-           url: aPath,
-           cache:false,
-           dataType: "html",
-           success: onLoadDataComplete,
-           error: onLoadError
-         });
-}
 
 $(function(){
   myTeamHelper.connectToLeaguePicker();
   
-  var theFirstFanzone = $("div#myTailgateList dl dd a")[0];
-  if (theFirstFanzone)
-  {
-    window.location = theFirstFanzone.href;
-  }
+  $("div#myTailgateList dl dd a").first().click();
 })
