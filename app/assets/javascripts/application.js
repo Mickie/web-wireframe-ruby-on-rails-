7ae9f0b94c6ef5be52d5e29d8df8def5846bbea0
Paul Ingalls <paul@paulingalls.com>
2012-06-29 14:57:04 -0700

    save location when authing with facebook or twitter

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,16 +39,46 @@ function createExtendedDelegate(anObject, aMethod, anArgumentExtensionArray)
     var theNewArguments = theArgsAsArray.concat(anArgumentExtensionArray)
     return aMethod.apply(anObject, theNewArguments);
   }
-  
+
   return theDelegate;
+}
+
+function setCookie(aName, aValue, aNumberOfDaysTillExpire)
+{
+  var theExpiration = "";
+  if (aNumberOfDaysTillExpire)
+  {
+    var theExpirationDate = new Date();
+    theExpirationDate.setDate(theExpirationDate.getDate() + aNumberOfDaysTillExpire);
+    theExpiration = "; expires=" + theExpirationDate.toUTCString();
+  }
+
+  var theCookieValue = escape(aValue) + theExpiration;
+
+  document.cookie = aName + "=" + theCookieValue;
+}
+
+function getCookie(aName)
+{
+  var i, theCurrentName, theCurrentValue, theCookieArray = document.cookie.split(";");
+  for ( i = 0; i < theCookieArray.length; i++)
+  {
+    theCurrentName = theCookieArray[i].substr(0, theCookieArray[i].indexOf("="));
+    theCurrentValue = theCookieArray[i].substr(theCookieArray[i].indexOf("=") + 1);
+    theCurrentName = theCurrentName.replace(/^\s+|\s+$/g, "");
+    if (theCurrentName == aName)
+    {
+      return unescape( theCurrentValue );
+    }
+  }
+  return null;
 }
 
 $(function()
 {
-  setTimeout( function()
+  setTimeout(function()
   {
-    window.scrollTo( 0, 1 );
-  }, 100 );
+    window.scrollTo(0, 1);
+  }, 100);
 });
-
 
--- app/assets/javascripts/modules/left_nav.js
+++ app/assets/javascripts/modules/left_nav.js
@@ -4,11 +4,23 @@ var myTeamHelper = new TeamHelper("#league_picker", "#tailgate_team_id");
 $(function(){
   myTeamHelper.connectToLeaguePicker();
   initializeFrameContent();
+  initializeNavigationWatchers();
 });
 
 function initializeFrameContent()
 {
-  var theHash = window.location.hash;
+  var theHash = "";
+  var theLocationCookie = getCookie("myCurrentLocationHash");
+  if (theLocationCookie && theLocationCookie.length > 0)
+  {
+    theHash = theLocationCookie;
+    setCookie("myCurrentLocationHash", "");
+  }
+  else
+  {
+    theHash = window.location.hash;
+  }
+  
   if(theHash.length > 0)
   {
     var theLeftNavElement = $(theHash + ' a');
@@ -34,6 +46,24 @@ function getIdFromHash( aHash )
 {
   var thePieces = aHash.split("_");
   return thePieces[thePieces.length-1];
+}
+
+function saveLocation()
+{
+  setCookie("myCurrentLocationHash", window.location.hash);
+}
+
+function clearLocation() 
+{
+  setCookie("myCurrentLocationHash", "");
+}
+
+function initializeNavigationWatchers()
+{
+  $('#myLoginModal').on('shown', saveLocation);
+  $('#myLoginModal').on('hidden', clearLocation);  
+  $('#myConnectTwitterModal').on('shown', saveLocation);
+  $('#myConnectTwitterModal').on('hidden', clearLocation);  
 }
 
 function onLoadDataComplete(aResult)
