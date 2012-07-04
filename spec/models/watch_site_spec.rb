e15209a7d12f7a1dcf72b1e45312683befafe9e2
Paul Ingalls <paul@paulingalls.com>
2012-07-04 16:44:12 -0700

    fixed bug with null lat and lon for watch sites

--- spec/models/watch_site_spec.rb
+++ spec/models/watch_site_spec.rb
@@ -11,4 +11,18 @@ describe WatchSite do
   it { should respond_to(:name) }
   it { should respond_to(:venue) }
   it { should respond_to(:team) } 
+  
+  it "should store latitude and longitude" do
+    @watch_site.save
+    @watch_site.latitude.should_not be_nil
+    @watch_site.longitude.should_not be_nil
+  end
+
+  it "should store latitude and longitude when venue already exists" do
+    @watch_site.venue.save
+    @watch_site.save
+    @watch_site.latitude.should_not be_nil
+    @watch_site.longitude.should_not be_nil
+  end
+
 end
