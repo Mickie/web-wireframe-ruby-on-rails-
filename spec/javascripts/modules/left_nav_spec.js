4e0706b25d503ec4452467510d027aa39abd139e
Paul Ingalls <paul@paulingalls.com>
2012-07-10 23:59:45 -0700

    fixed some left over nav bugs

--- spec/javascripts/modules/left_nav_spec.js
+++ spec/javascripts/modules/left_nav_spec.js
@@ -2,16 +2,22 @@ describe("left_nav.js", function()
 {
   describe("getIdFromHash", function()
   {
-    it("should return 12 for #nav_fanzone_12", function()
+    it("should return 12 for #nav_12", function()
     {
-      var theResult = getIdFromHash("#nav_fanzone_12");
+      var theResult = getIdFromHash("#nav_12");
       expect(theResult).toEqual("12");
     });
 
-    it("should return 9 for #nav_fanzone_9", function()
+    it("should return 9 for #nav_9", function()
     {
-      var theResult = getIdFromHash("#nav_fanzone_9");
+      var theResult = getIdFromHash("#nav_9");
       expect(theResult).toEqual("9");
+    });
+
+    it("should return irish-white-knucklers for #nav_irish-white-knucklers", function()
+    {
+      var theResult = getIdFromHash("#nav_irish-white-knucklers");
+      expect(theResult).toEqual("irish-white-knucklers");
     });
   });
 });
