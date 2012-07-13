de905ad5c192820ecbec76f6a732085536d625bf
Paul Ingalls <paul@paulingalls.com>
2012-07-13 16:10:17 -0700

    no need to navToAllFanzones, we are already there.  Just make it look that way...

--- app/assets/javascripts/static_pages/home.js
+++ app/assets/javascripts/static_pages/home.js
@@ -2,6 +2,8 @@ $(function()
 {
   if (!window.location.hash || window.location.hash.length <= 1)
   {
-    navToAllFanzones();
+    $('#allFanzones').addClass('active');
+    InfiniteScroller.get().handleScrollingForResource("/tailgates");
+    $("#fanzone_search").slideDown(400);
   }
 });
