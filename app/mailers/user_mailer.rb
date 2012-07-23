813d0a3b15aea3970a2abef1e08af768b9fe908c
Paul Ingalls <paul@paulingalls.com>
2012-07-23 11:40:21 -0700

    send to real people, not just me...;)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,8 +9,8 @@ class UserMailer < ActionMailer::Base
     theSubject = "#{@post.user.full_name} just posted on #{@post.tailgate.name}"
     theToAddress = "#{@post.tailgate.user.full_name} <#{@post.tailgate.user.email}>"
     attachments.inline['fanzo-logo.png'] = File.read(Rails.root.join("app", "assets", "images", "fanzo-logo.png"))
-    mail( to: "paulingalls@hotmail.com, paulingalls@gmail.com, paul_ingalls@yahoo.com", subject: theSubject )
-#    mail( to: theToAddress, subject: theSubject )
+#    mail( to: "paulingalls@hotmail.com, paulingalls@gmail.com, paul_ingalls@yahoo.com", subject: theSubject )
+    mail( to: theToAddress, subject: theSubject )
   end
   
   def new_post_comment( aCommentId )
@@ -19,7 +19,7 @@ class UserMailer < ActionMailer::Base
     theSubject = "#{@comment.user.full_name} just commented on your post in #{@comment.post.tailgate.name}"
     theToAddress = "#{@comment.post.user.full_name} <#{@comment.post.user.email}>"
     attachments.inline['fanzo-logo.png'] = File.read(Rails.root.join("app", "assets", "images", "fanzo-logo.png"))
-    mail( to: "paulingalls@hotmail.com, paulingalls@gmail.com, paul_ingalls@yahoo.com", subject: theSubject )
-#    mail( to: theToAddress, subject: theSubject )
+#    mail( to: "paulingalls@hotmail.com, paulingalls@gmail.com, paul_ingalls@yahoo.com", subject: theSubject )
+    mail( to: theToAddress, subject: theSubject )
   end
 end
