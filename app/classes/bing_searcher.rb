3ca5d15e7b8224517f43cc1af94aec2e6a049fa2
Paul Ingalls <paul@paulingalls.com>
2012-08-20 21:03:36 -0700

    added in support for special version of fanzo founders

--- app/classes/bing_searcher.rb
+++ app/classes/bing_searcher.rb
@@ -4,32 +4,51 @@ class BingSearcher
     Team.visible.each do | aTeam |
       searchForTeamData( aTeam.id )
     end
+    
+    updateFanzoFounders()
+  end
+  
+  def updateFanzoFounders
+    theTeam = Team.find_by_name("Fanzo Founders")
+    theQuery = "'Latest Football Tailgate news'"
+    theResponse = getBingResultsForQuery( theQuery )
+    saveResponseToTeam( theResponse, theTeam )
   end
   
   def searchForTeamData( aTeamId )
     theTeam = Team.find(aTeamId)
+    theQuery = "'Latest news on #{theTeam.name} #{theTeam.sport.name}'"
+    theResponse = getBingResultsForQuery( theQuery )
+    saveResponseToTeam( theResponse, theTeam )
+  end
+  
+  def getBingResultsForQuery( aQuery )
+    
     theConnection = Faraday.new('https://api.datamarket.azure.com', ssl:{ ca_path: "/usr/lib/ssl/certs/ca-certificates.crt"})
-    theConnection.basic_auth("", "QxHn05Z96oTB5U46PZ/zbJ6hJeSztC2uhhmWDTGK6lQ=")
+    theConnection.basic_auth("", ENV["FANZO_BING_KEY"])
     
     theResponse = theConnection.get "/Data.ashx/Bing/Search/v1/Composite" do | aRequest |
       aRequest.params["Sources"] = "'web+image+video+news'"
-      aRequest.params["Query"] = "'Latest news on #{theTeam.name} #{theTeam.sport.name}'"
+      aRequest.params["Query"] = aQuery
       aRequest.params["NewsCategory"] = "'rt_Sports'"
       aRequest.params["$top"] = "25"
       aRequest.params["$format"] = "JSON"
     end
+  end
 
-    Rails.logger.info("bing search response for #{theTeam.name}: #{theResponse.status}")
-    if (theResponse.status == 200)
-      if theTeam.bing_search_results
-        theTeam.bing_search_results.search_result = theResponse.body
-        theTeam.bing_search_results.save
+  def saveResponseToTeam( aResponse, aTeam )
+    Rails.logger.info("bing search response for #{aTeam.name}: #{aResponse.status}")
+    if (aResponse.status == 200)
+      if aTeam.bing_search_results
+        aTeam.bing_search_results.search_result = aResponse.body
+        aTeam.bing_search_results.save
       else
-        theTeam.create_bing_search_results( search_result: theResponse.body )
-        theTeam.save
+        aTeam.create_bing_search_results( search_result: aResponse.body )
+        aTeam.save
       end
     else
-      Rails.logger.error("Failed to load bing search results")
+      Rails.logger.error("Failed to load bing search results for team: #{aTeam.name}")
     end
   end
+
 end
