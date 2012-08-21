894b93fa720fcfc72d823232400a4d09220a2970
Paul Ingalls <paul@paulingalls.com>
2012-08-21 15:37:38 -0700

    try to get logs working in prod

--- config/initializers/sitemap_logger.rb
+++ config/initializers/sitemap_logger.rb
@@ -1,4 +1,9 @@
-log_path = File.join(Rails.root, 'log/sitemap.log')
-log_file = File.open(log_path, 'a')
-log_file.sync = true
+if Rails.env.development?
+  log_path = File.join(Rails.root, 'log/sitemap.log')
+  log_file = File.open(log_path, 'a')
+  log_file.sync = true
+else
+  log_file = STDOUT    
+end
+
 SitemapLogger = Logger.new(log_file)
