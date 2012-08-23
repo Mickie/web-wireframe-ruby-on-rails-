2543b7dcfc745d7ad10ee46873644b1f86c6ebbb
Paul Ingalls <paul@paulingalls.com>
2012-08-23 14:20:55 -0700

    woops, missed making elements accessible

--- app/models/team.rb
+++ app/models/team.rb
@@ -31,7 +31,9 @@ class Team < ActiveRecord::Base
   accepts_nested_attributes_for :location
   accepts_nested_attributes_for :social_info
   
-  attr_accessible :name, 
+  attr_accessible :name,
+                  :short_name,
+                  :mascot,
                   :sport_id, 
                   :league_id, 
                   :division_id, 
