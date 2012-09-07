9057c55dc47c9032b4c015e81f8ee917dc677f7a
Paul Ingalls <paul@paulingalls.com>
2012-09-06 21:56:16 -0700

    fixes for ie8

--- app/assets/javascripts/globals.js
+++ app/assets/javascripts/globals.js
@@ -76,6 +76,30 @@ Function.prototype.subclass= function(base) {
 };
 Function.prototype.subclass.nonconstructor= function() {};
 
+Date.prototype.toISOString || (Date.prototype.toISOString = (function(){
+  
+  var str = function(n, l) {
+    var str = String(n),
+      len = l || 2;
+    while( str.length < len )
+      str = '0' + str;
+    return str;
+  };
+  
+  return function(){
+      return isFinite( this.getTime() )
+        ? String(this.getUTCFullYear()).concat( '-', 
+          str(this.getUTCMonth() + 1), "-",
+          str(this.getUTCDate()), "T",
+          str(this.getUTCHours()), ":",
+          str(this.getUTCMinutes()), ":",
+          str(this.getUTCSeconds()), ".",
+                      str(this.getUTCMilliseconds(),3), "Z" )
+                    : 'Invalid Date';
+    };
+  
+})() );
+
 $(function()
 {
   setTimeout(function()
