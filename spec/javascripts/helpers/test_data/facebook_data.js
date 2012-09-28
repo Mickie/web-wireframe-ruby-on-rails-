b1ea16af32fc840cd415350dcb6e13b39e96e0c0
Paul Ingalls <paul@paulingalls.com>
2012-09-28 12:03:09 -0700

    response for /me from facebook on client

---
+++ spec/javascripts/helpers/test_data/facebook_data.js
@@ -0,0 +1,140 @@
+var FacebookMeData = 
+{
+  "id": "690891536", 
+  "name": "Paul Ingalls", 
+  "first_name": "Paul", 
+  "last_name": "Ingalls", 
+  "link": "https://www.facebook.com/paulingalls", 
+  "username": "paulingalls", 
+  "hometown": {
+    "id": "111723635511834", 
+    "name": "Bellevue, Washington"
+  }, 
+  "location": {
+    "id": "105506992816118", 
+    "name": "Kirkland, Washington"
+  }, 
+  "work": [
+    {
+      "employer": {
+        "id": "328233513880061", 
+        "name": "Fanzo"
+      }, 
+      "location": {
+        "id": "105506992816118", 
+        "name": "Kirkland, Washington"
+      }, 
+      "position": {
+        "id": "130899310279007", 
+        "name": "Founder, CEO"
+      }, 
+      "description": "FANZO connects like minded sports fans", 
+      "start_date": "2012-02"
+    }, 
+    {
+      "employer": {
+        "id": "6289201895", 
+        "name": "Smilebox"
+      }, 
+      "location": {
+        "id": "109738839051539", 
+        "name": "Redmond, Washington"
+      }, 
+      "position": {
+        "id": "141310215902873", 
+        "name": "Vice President of Technology"
+      }, 
+      "description": "Smilebox is a fun and engaging service that connects you with family and friends using your photos and videos. It's free, easy to use, and the results are amazing.  I've had a ton of fun building it...", 
+      "start_date": "2005-01", 
+      "end_date": "2012-01"
+    }, 
+    {
+      "employer": {
+        "id": "103100096396281", 
+        "name": "RealNetworks"
+      }, 
+      "location": {
+        "id": "110843418940484", 
+        "name": "Seattle, Washington"
+      }, 
+      "position": {
+        "id": "143603992330852", 
+        "name": "Development Manager"
+      }, 
+      "description": "RealArcade", 
+      "start_date": "1997-12", 
+      "end_date": "2004-12"
+    }, 
+    {
+      "employer": {
+        "id": "114045225277986", 
+        "name": "Asymetrix"
+      }, 
+      "location": {
+        "id": "111723635511834", 
+        "name": "Bellevue, Washington"
+      }, 
+      "position": {
+        "id": "109542932398298", 
+        "name": "Software Engineer"
+      }, 
+      "description": "Fun times building 3D F/X, Web 3D, Digital Video Producer etc.", 
+      "start_date": "1995-05", 
+      "end_date": "1997-12"
+    }
+  ], 
+  "favorite_teams": [
+    {
+      "id": "155703994459155", 
+      "name": "Notre Dame Football"
+    }, 
+    {
+      "id": "34459843588", 
+      "name": "Seattle Sounders FC"
+    }, 
+    {
+      "id": "84249551721", 
+      "name": "Seattle Seahawks"
+    }, 
+    {
+      "id": "132551270119496", 
+      "name": "Notre Dame Fighting Irish"
+    }
+  ], 
+  "education": [
+    {
+      "school": {
+        "id": "109214499097306", 
+        "name": "Eastside Catholic School"
+      }, 
+      "year": {
+        "id": "131367623569918", 
+        "name": "1990"
+      }, 
+      "type": "High School"
+    }, 
+    {
+      "school": {
+        "id": "107332935975625", 
+        "name": "University of Notre Dame"
+      }, 
+      "year": {
+        "id": "135676686463386", 
+        "name": "1994"
+      }, 
+      "concentration": [
+        {
+          "id": "104076956295773", 
+          "name": "Computer Science"
+        }
+      ], 
+      "type": "College"
+    }
+  ], 
+  "gender": "male", 
+  "email": "paulingalls@gmail.com", 
+  "timezone": -7, 
+  "locale": "en_US", 
+  "verified": true, 
+  "updated_time": "2012-09-10T20:36:25+0000"
+}
