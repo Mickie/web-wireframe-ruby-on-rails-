52ac1fda7e2cedecb31083afa503888710f6298d
Paul Ingalls <paul@paulingalls.com>
2012-10-01 13:20:34 -0700

    made venue slider actually slide

--- app/assets/javascripts/modules/venueSlider.js
+++ app/assets/javascripts/modules/venueSlider.js
@@ -1,11 +1,13 @@
 var VenueSlider = function()
 {
-  this.myVenueSelector; 
+  this.myVenueSelector;
+  this.myScroller;
   
   this.initialize = function(aVenueSelector)
   {
     this.myVenueSelector = aVenueSelector;
     this.setupPicker();
+    this.setupScroller();
   }
   
   this.setupPicker = function()
@@ -14,6 +16,51 @@ var VenueSlider = function()
     $(this.myVenueSelector).find('.dropdown-menu').on("click", "a", createDelegate(this, this.onMenuSelect));
   }
   
+  this.setupScroller = function()
+  {
+    this.myScroller = new iScroll("venueScroller",
+                                  {
+                                    vScroll: false,
+                                    hScrollbar: false,
+                                    vScrollbar: false,
+                                    useTransform: true,
+                                    bounce: false,
+                                    snap: "div.venue"
+                                  }); 
+    EventManager.get().addObserver("onVenueListChanged", this);
+    
+    
+    $(this.myVenueSelector).hover(createDelegate(this, this.onHoverStart), createDelegate(this, this.onHoverEnd));
+    $(this.myVenueSelector + " div#scrollLeft").click( createDelegate(this, this.onScrollLeft ) );
+    $(this.myVenueSelector + " div#scrollRight").click( createDelegate(this, this.onScrollRight ) );
+  }
+  
+  this.onVenueListChanged = function()
+  {
+    this.myScroller.refresh();
+    this.myScroller.scrollToPage(0,0,5);
+  }
+  
+  this.onScrollLeft = function(e)
+  {
+    this.myScroller.scrollToPage("prev", 0, 600);
+  }
+  
+  this.onScrollRight = function(e)
+  {
+    this.myScroller.scrollToPage("next", 0, 600);
+  }
+  
+  this.onHoverStart = function(e)
+  {
+    $(this.myVenueSelector + " div#scrollerControls").fadeIn(500);
+  };
+  
+  this.onHoverEnd = function(e)
+  {
+    $(this.myVenueSelector + " div#scrollerControls").fadeOut(500);
+  };
+
   this.onMenuSelect = function(e)
   {
     if (e.target.parentNode.nodeName == "FORM")
